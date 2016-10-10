3403af5a177b182e216f34d3760408c30f3d8863
chaimturkel <chaim@tikalk.com>
2016-10-10 19:19:29 +0300

    remove code soultion

--- src/main/java/com/turel/rxjava/kafka/StreamRX.java
+++ src/main/java/com/turel/rxjava/kafka/StreamRX.java
@@ -21,24 +21,7 @@ public class StreamRX {
      * @return
      */
     public static <V> Observable<V> create(ConsumerIterator<byte[], byte[]> it, String topic, Deserializer<V> deserializer) {
-        return Observable.<V>create(subscriber -> {
-            try {
-                logger.info("start kafka stream");
-                while ((!subscriber.isUnsubscribed()) && it.hasNext()) {
-                    final V data = deserializer.deserialize(topic, it.next().message());
-                    subscriber.onNext(data);
-                }
-
-                if (!subscriber.isUnsubscribed()) {
-                    subscriber.onCompleted();
-                }
-
-                logger.info("stop kafka stream");
-            } catch (Exception e) {
-                if (!subscriber.isUnsubscribed())
-                    subscriber.onError(e);
-            }
-        });
+        return Observable.empty();
     }
 
 }
