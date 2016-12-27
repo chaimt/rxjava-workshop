a54e95fd1193c58c278b5d56bcf569ea5a5cf793
Chaim Turkel <cyturel@gmail.com>
2016-12-27 09:22:26 +0200

    add reduce examples

--- src/main/java/com/turel/rxjava/advanced/CombineExample.java
+++ src/main/java/com/turel/rxjava/advanced/CombineExample.java
@@ -15,18 +15,60 @@ public class CombineExample {
     /**
      * create two observable with range: 1-10, 20-30
      * merge the two to one observable
+     *
      * @return
      */
-    static public Observable<Integer> mergeWith(){
+    static public Observable<Integer> mergeWith() {
         return Observable.empty();
     }
 
     /**
      * create three observable with range: 1-10, 20-30, 50-60
      * merge the two to one observable
+     *
      * @return
      */
-    static public Observable<Integer> merge(){
+    static public Observable<Integer> merge() {
+        return Observable.empty();
+    }
+
+    /**
+     * create range from 1-5
+     * calulate the sum of them all
+     *
+     * @return
+     */
+    static public Observable<Integer> reduce() {
+        return Observable.empty();
+    }
+
+    /**
+     * create range from 1-5
+     * filter out so only one element is left and check if reduce works
+     *
+     * @return
+     */
+    static public Observable<Integer> reduceOne() {
+        return Observable.empty();
+    }
+
+    /**
+     * create range from 1-5
+     * filter out all elements and check if reduce works
+     *
+     * @return
+     */
+    static public Observable<Integer> reduceNull() {
+        return Observable.empty();
+    }
+
+    /**
+     * create range from 1-5
+     * filter out all elements and map to 0 with no error
+     *
+     * @return
+     */
+    static public Observable<Integer> reduceNullToZero() {
         return Observable.empty();
     }
 
@@ -35,14 +77,15 @@ public class CombineExample {
      * one from range 1-4
      * second from just a-d
      * zip then to -> s + "-" + i
+     *
      * @return
      */
-    static public Observable<String> zip(){
+    static public Observable<String> zip() {
         return Observable.empty();
     }
 
 
     public static void main(String[] args) {
-        Utils.runWithSubscription(log,mergeWith());
+        Utils.runWithSubscription(log, mergeWith());
     }
 }
--- src/test/java/com/turel/rxjava/advanced/CombineExampleTest.java
+++ src/test/java/com/turel/rxjava/advanced/CombineExampleTest.java
@@ -1,6 +1,5 @@
 package com.turel.rxjava.advanced;
 
-import com.turel.rxjava.error.ErrorExample;
 import org.junit.Assert;
 import org.junit.Test;
 import rx.observers.TestSubscriber;
@@ -50,6 +49,52 @@ public class CombineExampleTest {
 
     }
 
+    @Test
+    public void reduce(){
+        TestSubscriber subscriber = new TestSubscriber<>();
+
+        CombineExample.reduce().subscribe(subscriber);
+
+        subscriber.assertReceivedOnNext(Arrays.asList(15));
+        subscriber.assertNoErrors();
+        subscriber.assertTerminalEvent();
+    }
+
+    @Test
+    public void reduceOne(){
+        TestSubscriber subscriber = new TestSubscriber<>();
+
+        CombineExample.reduceOne().subscribe(subscriber);
+
+        subscriber.assertReceivedOnNext(Arrays.asList(5));
+        subscriber.assertNoErrors();
+        subscriber.assertTerminalEvent();
+
+    }
+
+    @Test
+    public void reduceNull(){
+        TestSubscriber subscriber = new TestSubscriber<>();
+
+        CombineExample.reduceNull().subscribe(subscriber);
+
+        subscriber.assertReceivedOnNext(Arrays.asList());
+        Assert.assertEquals(1,subscriber.getOnErrorEvents().size());
+        subscriber.assertTerminalEvent();
+
+    }
+
+    @Test
+    public void reduceNullToZero(){
+        TestSubscriber subscriber = new TestSubscriber<>();
+
+        CombineExample.reduceNullToZero().subscribe(subscriber);
+
+        subscriber.assertReceivedOnNext(Arrays.asList(0));
+        subscriber.assertNoErrors();
+        subscriber.assertTerminalEvent();
+
+    }
     @Test
     public void zip(){
         TestSubscriber subscriber = new TestSubscriber<>();
